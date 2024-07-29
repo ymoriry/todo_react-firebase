d710e3141793720076442cd1d1f5ad130e6d6bb2
omori <omr1414@icloud.com>
2024-07-29 15:09:20 +0900

    React - Tailwind - TypeScript - Firebaseの環境構築

---
+++ src/pages/todo/Todo.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import db from '../../lib/firebase/firebase'
+import {
+  Timestamp,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+
+const Todo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Todo
